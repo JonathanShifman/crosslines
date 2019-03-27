e80448651a39c9f90fae1590e6e176710484da01
Jonathan <jonathan.shifman@gmail.com>
2019-03-28 00:41:08 +0200

    First commit

---
+++ precache-manifest.94e7865cf9b5be28ab4a03a27fc1ea47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8a953c01d136cf5374f",
+    "url": "/crosslines/static/css/main.ed74ce3d.chunk.css"
+  },
+  {
+    "revision": "a8a953c01d136cf5374f",
+    "url": "/crosslines/static/js/main.97c3c3f1.chunk.js"
+  },
+  {
+    "revision": "c58c676d2cf3578d9fbd",
+    "url": "/crosslines/static/js/runtime~main.83a47936.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/crosslines/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "14f41d4c434e59156b040586db6421d0",
+    "url": "/crosslines/index.html"
+  }
+];
